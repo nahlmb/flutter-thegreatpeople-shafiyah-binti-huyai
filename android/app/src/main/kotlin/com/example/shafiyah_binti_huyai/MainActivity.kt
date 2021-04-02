4937920fa1aab740423f07e8886812633783d90f
Nahl <nahlultra@gmail.com>
2021-04-02 16:46:37 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/shafiyah_binti_huyai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shafiyah_binti_huyai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Shafiyah binti Huyai",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text(
+                  '''Ummul Mukminin Shafiyah binti Huyai memiliki silsilah nasab mulia. Ia seorang bangsawan Bani Nadhir keturunan Nabi Harun ‘alaihissalam. Artinya jika ditarik garis nasabnya, Nabi Musa ‘alaihissalam terhitung sebagai pamannya. Keutamaan itu semakin lengkap karena Nabi Muhammad shallallahu ‘alaihi wa sallam adalah suaminya.
+
+Nasabnya
+
+Beliau adalah Shafiyah binti Huyai bin Akhtab bin Syu’ah bin Ubaid bin Ka’ab bin al-Khazraj bin Abi Hubaib bin an-Nadhir bin an-Niham bin Tahum dari Bani Israil yang termasuk cucu Nabi Harun bin Imran ‘alaihissalam. Ibunya adalah Barrah binti Samau-al.
+
+Ummul Mukminin Shafiyah lahir tahun 9 sebelum hijrah dan wafat 50 H. Tahun lahir dan wafat itu bertepatan dengan 613 M dan 670 M.
+
+Sebelum menikah dengan Rasulullah shallallahu ‘alahi wa sallam, Ibunda Shafiyah bersuamikan Sallam bin Misykam. Ia adalah seorang penyair. Kemudian Sallam menceraikannya. Berikutnya ia menikah dengan Kinanah bin Abu Huqaiq yang juga merupakan seorang penyair. Di zaman itu, penyair menempati posisi mulia. Mereka terhitung sebagai cerdik cendekia. Dan suami terakhirnya tewas di Perang Khaibar (Ibnu Abdil Bar: al-Isti’ab, 4/1871 dan Muhibbuddin ath-Thabari: as-Simthu ats-Tsamin Hal: 201).
+
+Kaum Yang Membenci Risalah Rabbani
+
+Ummul Mukminin Shafiyah radhiallahu ‘anha adalah seorang bangsawan Bani Quraizhah dan Bani an-Nadhir. Ayahnya, Huyai bin Akhtab, adalah tokoh Yahudi sekaligus ulama mereka. Sang ayah tahu bahwa Muhammad bin Abdullah adalah seorang nabi akhir zaman. Ia tahu persis sejak kali pertama kaki Nabi yang mulia menjejak tanah Yatsrib (nama Madinah di masa lalu). Namun ia sombong dan menolak kebenaran. Karena apa? Karena nabi itu berasal dari Arab bukan dari anak turunan Israil (Nabi Ya’qub).
+
+Keadaan ayah Ummul Mukmini Shafiyah ini, langsung ia ceritakan sendiri. Ia bercerita, “Tak ada seorang pun anak-anak ayahku dan pamanku yang lebih keduanya cintai melebihi aku. Tak seorang pun anak-anak keduanya membuat mereka gembira, kecuali ia melibatkan aku bersamanya. Ketika Rasulullah shallallahu ‘alaihi wa sallam tiba di Quba -perkampungan Bani Amr bin Auf-, ayah dan pamanku, Abu Yasir bin Akhtab, datang menemuinya di pagi buta. Demi Allah, mereka baru pulang menemui kami saat matahari menghilang. Keduanya datang dengan ekspresi layu, lunglai, dan jalan tergontai lesu. Aku berusaha membuat mereka gembira seperti yang biasa kulakukan. Demi Allah, tak seorang pun dari keduanya peduli walau hanya sekadar menoleh padaku. Aku dengar pamanku, Abu Yasir, bicara pada ayahku, “Apakah dia itu memang si nabi itu? “Iya, demi Allah,” jawab ayah. “Kau kenali dia dari sifat-sifat dan tanda-tandanya?” tanya paman lagi. “Iya, demi Allah,” ayah memberikan jawaban yang sama. “Lalu bagaimana keadaan dirimu terhadapnya?” tanya paman. Ayah menjawab, “Demi Allah, permusuhan selama aku masih hidup.”
+
+Diceritakan oleh Musa bin Uqbah az-Zuhri bahwa saat Rasulullah shallallahu ‘alaihi wa sallam di Madinah, Abu Yasir bin Akhtab datang menemui beliau. Ia mendengar ucapan-ucapan beliau. Setelah itu, ia kembali ke kaumnya dan berkata, “Taatilah aku. Sesungguhnya Allah telah mendatangkan pada kalian seseorang yang kalian tunggu-tunggu. Ikutilah dia! Jangan kalian menyelisihinya.”
+
+Kemudian Huyai bin Akhtab yang merupakan pimpinan Yahudi segera beranjak menemui Rasulullah shallallahu ‘alaihi wa sallam. Ia duduk dan mendengar ucapan Nabi. Setelah itu pulang menuju kaumnya. Huyai adalah seorang yang ditaati. Ia berkata, “Aku telah datang menemui laki-laki itu. Demi Allah, aku akan senantiasa memusuhinya selamanya.”
+
+Abu Yasir berkata, “Hai anak pamanku, ikutlah bersamaku dalam permasalahan ini. Selain urusan ini silahkan tidak bersamaku sekehendakmu. Kau tidak akan binasa.”
+
+Huyai menjawab, “Tidak! Demi Allah! Aku tak akan menurutimu.” Ia dikuasai oleh setan. Dan kaumnya mengikuti pandangannya (Ibnu Hisyam: as-Sirah an-Nabawiyah 1/519-520 dan Ibnu Katsir: as-Sirah an-Nabawiyah 2/298).
+
+Pernikahan Terindah
+
+Sejak Nabi tiba di Madinah, orang-orang Yahudi Khaibar telah bulat menolak ajakan damai. Kebuntuan tersebut tak dapat didobrak kecuali dengan perang. Pecahlah perang antar dua kelompok yang akhirnya dimenangkan oleh umat Islam.
+
+Saat para tawanan dikumpulkan, Dihyah bin Khalifah al-Kalbi menemui Rasulullah shallallahu ‘alaihi wa sallam. Ia berkata, “Wahai Rasulullah, berilah aku seorang budak wanita dari tawanan ini.” Nabi menaggapi, “Silahkan. Ambillah seorang budak perempuan.” Dihyah yang Malaikat Jibril suka menyerupainya ini memilih Shafiyah binti Huyai.
+
+Lalu datang seseorang menemui Nabi. Ia berkata, “Wahai Nabi Allah, Anda telah memberikan Dihyah seorang Shafiyah binti Huyai. Seorang tokoh Bani Quraizah dan Bani Nadhir. Wanita yang tidak layak untuk siapapun kecuali Anda. Kemudian Nabi bersabda, “Panggil Dihyah bersama dengan Shafiyah.” Dihyah pun datang membawa Shafiyah. Saat Nabi melihat Shafiyah, beliau berkata pada Dihyah, “Pilihlah tawanan wanita selain dirinya.”
+
+Dalam kesempatan lain, Nabi shallallahu ‘alaihi wa sallam memerintahkan Bilal untuk membawa Shafiyah. Saat hendak menemui Nabi, Bilal membawa Shafiyah lewat di jalan yang banyak korban perang. Rasulullah tidak menyukai apa yang dilakukan Bilal. Beliau bersabda, “Apakah telah hilang rasa kasih sayang pada dirimu, Bilal?”
+
+Rasulullah menawari Shafiyah untuk memeluk Islam. Ia pun memilih Islam. Kemudian Nabi memperistrinya (setelah masa iddahnya selesai). Pembebasannya sebagai tawanan adalah mahar pernikahannya. Saat memandangnya, Nabi melihat ada bekas lebam di wajah Shafiyah. Beliau bertanya, “Apa ini?” Shafiyah menjawab, “Sebelum kedatanganmu, aku bermimpi seakan rembulan hilang dari tempatnya dan masuk ke rumahku. Demi Allah, saat itu aku sama sekali tidak menyebut-nyebut dirimu. Kemudian kuceritakan mimpi itu pada suamiku. Ia pun menamparku.” Ia berkata, “Kau berharap penguasa Madinah itu!” (Ibnul Qayyim: Zadul Ma’ad, 3/291)
+
+Salah satu hikmah dari berbilangnya istri Rasulullah shallallahu ‘alaihi wa sallam adalah faktor sosial. Yaitu menolong dan menjaga perasaan orang yang dinikahi. Agama Islam menjaga kedudukan seseorang. Dengan Islam kedudukan mereka tidak terjatuh, bahkan semakin mulia. Siapa yang mulia sebelum memeluk Islam, semakin mulia kedudukannya dengan Islam. Demikian juga keadaan Ummul Mukminin Shafiyah binti Huyai radhiallahu ‘anha. Ia adalah wanita mulia sebelum memeluk Islam. Seorang pemuka kaumnya. Putri dari kepala kabilah. Dan istri seorang yang mulia pula. Setelah ia memeluk Islam, agama yang mulia ini tetap menjaga kedudukannya. Menjaga perasaannya. Allah nikahkan dia dengan orang paling mulia di tengah-tengah kaum muslimin, Rasulullah shallallahu ‘alaihi wa sallam.
+
+Rasulullah Memuliakan Shafiyah
+
+Rasulullah adalah seorang yang lembut dan penyayang kepada istri-istrinya, termasuk kepada Ummul Mukminin Shafiyah radhiallahu ‘anha. Suatu hari, sampai di telinga Shafiyah bahwa istri nabi, Hafshah binti Umar, menyebutnya dengan putri seorang Yahudi. Shafiyah menangis. Saat bersamaan Rasulullah shallallahu ‘alaihi wa sallam datang menemuinya dan melihatnya menangis. Beliau bertanya, “Apa yang membuatmu menangis?” Ia menjawab, “Hafshah binti Umar berkata padaku bahwa aku adalah putri seorang Yahudi.” Nabi berkata padanya, “Sesungguhnya engkau adalah putri seorang nabi. Pamanmu pun seorang nabi. Dan engkau dalam naungan seorang nabi. Bagaimana kau tidak bangga dengan hal itu.” Kemudian beliau berkata pada Hafsha, “Wahai Hafshah, bertakwalah kepada Allah.” (Muhibbuddin ath-Thabari: as-Shimthu ats-Tsamin Hal 207).
+
+Maksud Shafiyah putri seorang nabi adalah nasabnya yang sampai Nabi Harun. Sehingga Nabi Harun terhitung sebagai ayahnya. Dan Nabi Harun merupakan saudara Nabi Musa. Sehingga ia memiliki paman seorang nabi juga. Tentang di bawah naungan nabi. Maksudnya suamimu yang menaungimu pun nabi.
+
+Suatu hari Nabi shallallahu ‘alaihi wasallam beri’tikaf di masjid. Nabi bersama istri-istrinya. Saat mereka pergi, Nabi berkata kepada Shafiyah binti Huyay, “Jangan tergesa-gesa pulang. Akan kuantar engkau.” Rumah Shafiyah berada di tempatnya Usamah. Nabi shallallahu ‘alaihi wa sallam keluar bersama Shafiyah. Di jalan, Nabi bertemu dua orang Anshar. Keduanya memandang Nabi shallallahu ‘alaihi wasallam sesaat lalu terus berjalan. Beliau shallallahu ‘alaihi wasallam berkata pada keduanya, “Kemarilah kalian, ini adalah Shafiyah binti Huyay”. Maka keduanya berkata, “Maha suci Allah, wahai Rasulullah”. Lalu Beliau shallallahu ‘alaihi wasallam bersabda, “Sesungguhnya syetan berjalan pada diri manusia lewat aliran darah dan aku khawatir telah timbul suatu perasaan pada diri kalian berdua.” (HR. Al-Bukhari No. 1897 Kitab I’tikaf)
+
+Wafat
+
+Ummul Mukminin Shafiyah binti Huyai radhiallahu ‘anha wafat pada tahun 50 H/670 M di zaman pemerintahan Muawiyah bin Abu Sufyan radhiallahu ‘anhu. Dan beliau dimakamkan di Pemakaman Baqi’ (Ibnu Hajar: Tadzhib at-Tadzhib, 12/380).
+
+
+
+Read more https://kisahmuslim.com/6209-ummul-mukminin-shafiyah-binti-huyai.html#more-6209'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : kisahmuslim.com",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
